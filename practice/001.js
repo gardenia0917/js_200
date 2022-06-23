bdee80f9aa99c335d09126572e8e4fabcc0d54e6
gardenia1109 <gardenia1109@naver.com>
2022-06-23 20:48:38 +0900

    001

---
+++ practice/001.js
@@ -0,0 +1,9 @@
+console.log("Hello World!! 🔥");
+
+// 콘솔창에 넣어보기
+document.createElement("div"); // div 생성하기
+// <div></div>
+let element_div = document.createElement("div"); // 변수 element_div에 div 삽입?
+// undefined ▶️ 브라우저 내부 메모리에 변수 div를 저장했기 때문
+element_div.id = `div_name`; //id 가 div_name
+// 'div_name'
