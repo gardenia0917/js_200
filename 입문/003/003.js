5e2ed1c6a36fef0651f5198d7db503c2a5be4a9e
gardenia1109 <gardenia1109@naver.com>
2022-06-23 22:31:06 +0900

    004

--- 입문/003/003.js
+++ 입문/003/003.js
@@ -1,3 +1,4 @@
+// Live Server
 console.log("VSC 실행해보기 1");
 let a = 5;
 let b = 2;
---
+++ 입문/004/004.js
@@ -0,0 +1,7 @@
+// Run Code
+console.log("VSC 2");
+let a = 12;
+let b = 13;
+console.log(a + b);
+// VSC 2
+// 25
